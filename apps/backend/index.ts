0b7b0a6b06995a94dabab73a45fcb594c29b4cb2
mahendraDew <mahendradewangan195@gmail.com>
2025-12-22 17:10:10 +0530

    package:db -done

---
+++ apps/backend/index.ts
@@ -0,0 +1,43 @@
+import express from "express";
+import { prismaClient } from "db/client";
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/users", (req, res) => {
+  prismaClient.user.findMany()
+    .then((users: any) => {
+      res.json(users);
+    })
+    .catch((err: any) => {
+      res.status(500).json({ error: err.message });
+    });
+})
+
+app.post("/user", (req, res) => {
+  const { username, password } = req.body;
+  
+  if (!username || !password) {
+    res.status(400).json({ error: "Username and password are required" });
+    return
+  }
+
+  prismaClient.user.create({
+    data: {
+      username,
+      password
+    } 
+  })
+    .then((user: any) => {
+      res.status(201).json(user);
+    })
+    .catch((err: any) => {
+      res.status(500).json({ error: err.message });
+    });
+})
+
+app.listen(3000, () => {
+    console.log("server is runnin on port 3000....");
+    
+});
